d434598fafea0f6cfdc1131a1a21ddc71dc0401f
Yevhen Odynets <6lo5jp@proton.me>
2026-08-17 11:06:31 +0300

    chore: untrack composer.phar, composer.bat, composer.lock
    The composer runtime files live in the working copy only: they are now ignored (/composer.lock, /composer.phar, *.bat) and removed from the index. composer.json stays tracked.

--- composer.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if not exist composer.phar (
-    echo Downloading composer.phar...
-    php -r "readfile('https://getcomposer.org/installer');" | php
-    php composer.phar install
-)
-C:\php\8.5.8\php -d xdebug.mode=off composer.phar %*
